44391c0e842696af12b5caa8fa2296d458123c55
Vipul Kumar Sharma <vipul_sharma86@optum.com>
2021-05-15 10:11:20 +0530

    first commit

---
+++ src/main/java/io/home/exploretheweb/reactivetesting/ReactiveTestingApplication.java
@@ -0,0 +1,13 @@
+package io.home.exploretheweb.reactivetesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveTestingApplication.class, args);
+	}
+
+}
